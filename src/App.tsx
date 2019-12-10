69b52b1a5e6e2364227c621fb48fdc852740cef9
Kenton Lam <kenton_lam@outlook.com>
2019-12-11 08:30:51 +1000

    Using CSS grid.

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import _ from 'lodash';
+
 
 import DayColumn from './DayColumn';
 import TimeColumn from './TimeColumn';
 
 const App: React.FC = () => {
+  const makeTimes = () => [
+    <th className="col-time has-text-centered is-hidden-tablet">Time</th>,
+    ..._.range(15).map(i => 
+      <th className="col-time has-text-centered is-hidden-tablet">{i+8}</th>)
+  ];
+  
+  const makeDay = (d: number) => [
+    <th>Monday {d}</th>,
+    ..._.range(15).map((i) => 
+      <td >{"jio emieov mfweio mveiow mvefmqvio qemvioqwm oiCourse "+i}</td>)
+  ];
+
   return (
     <div className="container">
       <div className="section">
         <h1 className="title">UQ Toilet Paper 🧻</h1>
         <p className="block">So you can plan your timetable in peace. Responsive!</p>
-        <div style={{flexWrap: 'wrap'}} className="table rtable">
-          <th style={{order: 0, flexGrow: 20}} className="has-text-centered">Time</th>
+        <div className="table timetable is-bordered">
+          <th className="has-text-centered is-hidden-mobile">Time</th>
           {Array.from(Array(15).keys())
-            .map((i) => <th style={{order: i+1, flexGrow: 20}} className="has-text-centered">{i+8}</th>)}
+            .map((i) => <th className="has-text-centered is-hidden-mobile">{i+8}</th>)}
 
-          <div className="rtable-spacer"></div>
           
-          <th style={{order: 0}}>Monday</th>
-          {Array.from(Array(15).keys())
-            .map((i) => <td style={{order: i+1}}>{"jio emieov mfweio mveiow mvefmqvio qemvioqwm oiCourse "+i}</td>)}
-            
-            <div className="rtable-spacer"></div>
-          <th style={{order: 0}}>Tuesday</th>
-          {Array.from(Array(15).keys())
-            .map((i) => <td style={{order: i+1}}>{"Course "+i}</td>)}
-<div className="rtable-spacer"></div>
-          <th style={{order: 0}}>Tuesday</th>
-          {Array.from(Array(15).keys())
-            .map((i) => <td style={{order: i+1}}>{"Course "+i}</td>)}
-<div className="rtable-spacer"></div>
-          <th style={{order: 0}}>Tuesday</th>
-          {Array.from(Array(15).keys())
-            .map((i) => <td style={{order: i+1}}>{"Course "+i}</td>)}
-<div className="rtable-spacer"></div>
-          <th style={{order: 0}}>Tuesday</th>
-          {Array.from(Array(15).keys())
-            .map((i) => <td style={{order: i+1}}>{"Course "+i}</td>)}
-<th style={{order: 0}}>Tuesday</th>
-          {Array.from(Array(15).keys())
-            .map((i) => <td style={{order: i+1}}>{"Course "+i}</td>)}
-<div className="rtable-spacer"></div>
-<th style={{order: 0}}>Tuesday</th>
-          {Array.from(Array(15).keys())
-            .map((i) => <td style={{order: i+1}}>{"Course "+i}</td>)}
-<div className="rtable-spacer"></div>
-          {Array.from(Array(16).keys())
-            .map((i) => <div className="rtable-break" style={{order: i}}></div>)}
+          {_.zip(makeTimes(), makeDay(0))}
+          <div className="rtable-spacer"></div>
+          {(_.zip(makeTimes(), makeDay(1)))}
+          <div className="rtable-spacer"></div>
+          {(_.zip(makeTimes(), makeDay(2)))}
+          <div className="rtable-spacer"></div>
+          {(_.zip(makeTimes(), makeDay(3)))}
+          <div className="rtable-spacer"></div>
+          {(_.zip(makeTimes(), makeDay(4)))}
+          <div className="rtable-spacer"></div>
+          {(_.zip(makeTimes(), makeDay(5)))}
+          <div className="rtable-spacer"></div>
+          {(_.zip(makeTimes(), makeDay(6)))}
         </div>
         <div className="block">Below is the Bulma columns implementation.</div>
         <div className="columns is-gapless">
-          <div className="column is-hidden-mobile is-1">
+          <div className="column is-hidden-mobile is-1" style={{width: '4rem'}}>
             <TimeColumn></TimeColumn>
           </div>
-          {Array.from(Array(5).keys()).map(i => 
+          {Array.from(Array(7).keys()).map(i => 
             <div className="column"><DayColumn></DayColumn></div>)}
         </div>
         <div className="table-container">
