3ed306643dc04021d732c984d08d33bd64abf413
Kenton Lam <kenton_lam@outlook.com>
2019-12-13 23:36:05 +1000

    Playing with styling.

--- src/Timetable.tsx
+++ src/Timetable.tsx
@@ -13,23 +13,30 @@ const START_HOUR = 8;
 const END_HOUR = 18;
 
 const sessionStyle = css({
-    backgroundColor: 'red',
     flexGrow: 1,
     flexBasis: 0,
     wordBreak: 'break-all',
 
-    paddingTop: '0.5em',
+    // paddingTop: '0.5em',
     //lineHeight: 1,
 
-    // display: 'flex',
-    // flexDirection: 'column',
-    // justifyContent: 'flex-start',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignContent: 'center',
+    flexWrap: 'wrap',
+
+
     marginLeft: '2px',
     marginRight: '2px' ,
 
     borderRadius: '0px',
     borderLeft: '1px solid #dbdbdb',
     borderRight: '1px solid #dbdbdb',
+    
+    backgroundColor: 'hsl(0, 0%, 96%)',
+
+    color: 'black',
 });
 
 type TimetableSessionProps = {
@@ -37,16 +44,31 @@ type TimetableSessionProps = {
 }
 
 const TimetableSession = ({session}: TimetableSessionProps) => {
-    return <div key={makeSessionKey(session)} className={"has-background-light  " + sessionStyle}>
-        <b>{getCourseCode(session.course)}</b><br></br>
-        {session.activity} {session.group}
-    </div>;
+    const activityCSS: {[type: string]: string} = {
+        'LEC': 'is-info',
+        'TUT': 'is-success',
+        'PRA': 'is-warning',
+    };
+
+    return <a key={makeSessionKey(session)} className={" " + sessionStyle}>
+        <span className="is-family-monospace has-text-weight-bold">{getCourseCode(session.course)}</span>
+        <div className="tags has-addons has-text-weight-semibold	">
+            <span className={"tag " + (activityCSS[session.activityType]) ?? "" }>{session.activity}</span>
+            <span className="tag is-dark ">{session.group}</span>
+        </div>
+    </a>;
 }
 
+const timeStyle = css({
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+})
+
 const makeTimeElements = (desktop: boolean) => [
-    <div key="ht" className={"th thead has-text-right " + (!desktop?"is-hidden-tablet":"is-hidden-mobile")}>Time</div>,
+    <div key="ht" className={"th thead has-text-right " + (!desktop?"is-hidden-tablet":"is-hidden-mobile")}></div>,
     ..._.range(START_HOUR, END_HOUR+1).map(h =>
-        <div key={"t"+h} className={"th has-text-right " + (!desktop?"is-hidden-tablet":"is-hidden-mobile")}>{h}</div>)
+        <div key={"t"+h} className={"th has-text-right is-size-5 " + timeStyle + ' ' + (!desktop?"is-hidden-tablet":"is-hidden-mobile")}>{h}</div>)
 ];
 
 const hourStyle = css({
@@ -70,14 +92,15 @@ type DayColumnProps = {
 const DayColumn = ({day, daySessions}: DayColumnProps) => {
     const timeColumn = makeTimeElements(false);
     return <>
-        <div className="th thead has-text-centered">{DAY_NAMES[day]}</div>
+        <div className="th thead has-text-centered is-size-5 ">{DAY_NAMES[day]}</div>
         {_.range(START_HOUR, END_HOUR+1).map((h) =>
             <React.Fragment key={h}>
                 {timeColumn[h]}
                 <div className={"td has-text-centered " + hourStyle}>
                     {daySessions[h].map(s => <TimetableSession session={s}></TimetableSession>)}
                 </div>
-            </React.Fragment>)}</>;
+            </React.Fragment>)}
+    </>;
 };
 
 const Timetable: React.FC<Props> = ({selectedSessions}) => {
