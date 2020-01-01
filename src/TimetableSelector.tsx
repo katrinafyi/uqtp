9824a411306c679fe2fa87eaf61c3e6e52f6ce13
Kenton Lam <kenton_lam@outlook.com>
2020-01-01 22:44:49 +1000

    Working on timetable button design.

--- src/TimetableSelector.tsx
+++ src/TimetableSelector.tsx
@@ -1,5 +1,5 @@
 import React, { useState, ButtonHTMLAttributes, DetailedHTMLProps } from "react"
-import { FaSave, FaPencilAlt, FaCopy, FaTimes } from "react-icons/fa";
+import { FaSave, FaPencilAlt, FaCopy, FaTimes, FaPlus } from "react-icons/fa";
 
 export type TimetableSelectorProps = {
     timetableNames: string[],
@@ -11,9 +11,9 @@ export const TimetableSelector = ({ timetableNames, current }: TimetableSelector
 
     const [isRenaming, setIsRenaming] = useState(false);
 
-    const makeTag = (x: string) => <div className="control"><div key={x} className="buttons has-addons">
+    const makeTag = (x: string) => <div className="control" key={x}><div key={x} className="buttons has-addons">
         <button className={"button  is-small " + (current === x ? 'is-link' : 'is-dark')}>{x}</button>
-        <button className="button  is-small is-outlined"><FaTimes></FaTimes></button>
+        <button className="button  is-small is-outlined"><span className="icon"><FaTimes></FaTimes></span></button>
     </div></div>;
 
     const onClickRename = (ev: React.MouseEvent<HTMLElement>) => {
@@ -37,10 +37,10 @@ export const TimetableSelector = ({ timetableNames, current }: TimetableSelector
                         <input type="text" className="input  " defaultValue={current}/>
                     </div>
                     <div className="control">
-                        <button className="button  is-success" type="submit" onClick={onClickRename}><FaPencilAlt></FaPencilAlt></button>
+                        <button className="button  is-success" type="submit" onClick={onClickRename}><span className="icon"><FaPencilAlt></FaPencilAlt></span></button>
                     </div>
                     <div className="control">
-                        <button className="button   is-link"><FaCopy></FaCopy></button>
+                        <button className="button   is-link"><span className="icon"><FaCopy></FaCopy></span></button>
                     </div>
                 </div>
             </div>
@@ -49,7 +49,10 @@ export const TimetableSelector = ({ timetableNames, current }: TimetableSelector
             <label className="label">Saved Timetables</label>
             <div className="control">
                 <div className="field is-grouped is-grouped-multiline">
-                    {makeTag("new timetable")}
+                    <div className="control">
+                        <button className="button is-link is-small"><span className="icon"><FaPlus></FaPlus></span></button>
+                    </div>
+                        
                     {timetableNames.map(makeTag)}
                 </div>
             </div>
